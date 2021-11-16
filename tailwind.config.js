90a625bfb54e7f3ef6b06fcb92a54d2a6f5c5316
David Keyes <david@rfortherestofus.com>
2021-11-15 21:12:45 -0800

    Basic site

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.njk', './src/**/*.md',],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+}
